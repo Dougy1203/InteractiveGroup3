2fabdb8f5fca560d2fec397b83e8bf35cd0b9572
AidanGull <78240873+AidanGull@users.noreply.github.com>
2021-12-06 13:42:04 -0700

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -11,5 +11,10 @@ let fetchData = async (url) => {
     const data = await response.json();
     console.log(data);
 };
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 fetchData('http://localhost:3000/api');
